3abc4cdacd839e0b5c898e1677137b6649de1fca
DarekP <darek.piechota@gmail.com>
2023-11-30 13:47:22 +0100

    02_06 Chalange starting point

---
+++ Chalanging/2_6 Chalange Date counter/2_6_chalange_date_counter/src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div clssName="App">
+      <Counter />
+    </div>
+  );
+}
+
+function Counter() {
+  return <div>Test</div>;
+}
