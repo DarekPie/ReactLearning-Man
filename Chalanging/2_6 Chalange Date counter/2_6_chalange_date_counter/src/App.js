7ea707dcabe79ad61d17157ae2630c5c81429517
DarekP <darek.piechota@gmail.com>
2023-12-04 15:42:54 +0100

    05-travel-list  Building form and handling list

--- Chalanging/2_6 Chalange Date counter/2_6_chalange_date_counter/src/App.js
+++ Chalanging/2_6 Chalange Date counter/2_6_chalange_date_counter/src/App.js
@@ -1,13 +1,42 @@
-import "./styles.css";
+import "./styles.css"
+import { useState } from "react"
 
 export default function App() {
   return (
     <div clssName="App">
       <Counter />
     </div>
-  );
+  )
 }
 
 function Counter() {
-  return <div>Test</div>;
+  const [count, setCount] = useState(0)
+  const [step, setStep] = useState(1)
+
+  const date = new Date("june 21 2027")
+  date.setDate(date.getDate() + count)
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => setStep((c) => c - 1)}>-</button>
+        <span>Step : {step}</span>
+        <button onClick={() => setStep((c) => c + 1)}>+</button>
+      </div>
+      <div>
+        <button onClick={() => setCount((c) => c - step)}>-</button>
+        <span>Count : {count}</span>
+        <button onClick={() => setCount((c) => c + step)}>+</button>
+      </div>
+
+      <span>
+        {count === 0
+          ? "Today is"
+          : count > 0
+          ? `${count} days from today is `
+          : `${Math.abs(count)} days ago was`}
+      </span>
+      <span>{date.toDateString()}</span>
+    </div>
+  )
 }
