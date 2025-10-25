ff4ddebc3fa388eeecf8c77489130fddf55f7cc3
DarekLap <darek.piechota@gmail.com>
2025-10-25 13:34:43 +0200

    quiz continue

--- NextSteps/13 Demo Project - React Quiz/01-starting-project/src/components/Quiz.jsx
+++ NextSteps/13 Demo Project - React Quiz/01-starting-project/src/components/Quiz.jsx
@@ -3,5 +3,8 @@ import {useState} from "react";
 
 export default function Quiz() {
 
+    const [activeQuestionIndex, setActiveQuestionIndex] = useState(0);
+    const [userAnswerss, setUserAnswerss] = useState([]); 
+
     return <p>Currently actie quueesstti</p>
 }
