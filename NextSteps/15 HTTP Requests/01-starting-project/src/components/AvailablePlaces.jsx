67f57125f533683df1cea1c7fe2c6b026e83ad6d
DarekLap <darek.piechota@gmail.com>
2026-01-20 12:33:08 +0100

    Fetching data to backedn

--- NextSteps/15 HTTP Requests/01-starting-project/src/components/AvailablePlaces.jsx
+++ NextSteps/15 HTTP Requests/01-starting-project/src/components/AvailablePlaces.jsx
@@ -1,15 +1,72 @@
-import { useState } from 'react';
-import Places from './Places.jsx';
+import { useEffect, useState } from "react";
+import Places from "./Places.jsx";
+import { use } from "react";
+import Error from "./Error.jsx";
+// import { sortedPlacesByDistance } from "../loc.js";
+import { sortPlacesByDistance } from "../loc.js";
+import { fetchAvailablePlaces } from "../http.js";
 
 // const places =  localStorage.getItem('places');
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    async function fetchPlaces() {
+      setIsFetching(true);
+
+      try {
+        // const response = await fetch("http://localhost:3000/places");
+        // const resData = await response.json();
+
+        // if (response.ok === false) {
+        //   const error = new Error("Failed to fetch places");
+        //   throw error;
+        // }
+        const places = await fetchAvailablePlaces();
+        
+        navigator.geolocation.getCurrentPosition((position) => {
+          console.log(position);
+          const sortedPlaces = sortPlacesByDistance(
+            places,
+            position.coords.latitude,
+            position.coords.longitude,
+          );
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
+
+        // setAvailablePlaces(resData.places);
+      } catch (error) {
+        setError({
+          message:
+            error.message || "Could not fetch places, please try again later.",
+        });
+      }
+    }
+    // fetch("http://localhost:3000/places")
+    //   .then((response) => {
+    //     return response.json();
+    //   })
+    //   .then((resData) => {
+    //     setAvailablePlaces(resData.places);
+    // });
+
+    fetchPlaces();
+  }, []);
+
+  if (error) {
+    return <Error title="An error occurred!" message={error.message} />;
+  }
 
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={false}
+      loadingText="Loading places data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
