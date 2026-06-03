67ec21fafd6f67480b501b29355ff378c2a1b0bc
DarekLap <darek.piechota@gmail.com>
2026-06-03 12:55:02 +0200

    Meal fetching

---
+++ NextSteps/18 Practice Project - Food Order/01-starting-project/src/components/Meals.jsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+
+export default function Meals() {
+  const [loadedMeals, setLoadedMeals] = useState([]);
+
+  useEffect(() => {
+    async function fetchMeals() {
+      // get is default method, so we don't need to specify it here
+      const response = await fetch("http://localhost:3000/meals");
+
+      if (!response.ok) {
+        // ... handle error
+      }
+      const meals = await response.json();
+      setLoadedMeals(meals);
+    }
+    fetchMeals();
+  }, []);
+
+  return (
+    <ul id="meals">
+      {loadedMeals.map((meal) => (
+        <li key={meal.id}>{meal.name}</li>
+      ))}
+    </ul>
+  );
+}
